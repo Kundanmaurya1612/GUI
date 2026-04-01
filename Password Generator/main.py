63cc418812ecd49ef9f9f89c58758048a0470f3a
Kundan <mauryakundan1612@gmail.com>
2026-04-01 17:40:05 +0530

    Add functionality to save and search passwords in JSON format

--- Password Generator/main.py
+++ Password Generator/main.py
@@ -1,6 +1,7 @@
 from tkinter import * # type: ignore
 from tkinter import messagebox
 import random
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -26,13 +27,46 @@ def save_data():
     name = ename.get()
     email = eemail.get()
     password = epassword.get()
+    data = {
+        name: {
+            "Email": email,
+            "Password": password
+        }
+    }
     if name != "" and email != "" and password != "":
         is_yes = messagebox.askyesno("Info","Data will be saved in data.txt\n\n       Want to save?")
         if is_yes:
-            with open("./Password Generator/data.txt", "a") as file:
-                file.write(f"\n\nName/website: {name}\nEmail: {email}\nPassword: {password}")
+            try:
+                with open("./Password Generator/data.json", "r") as file:
+                    # Read old data
+                    json_data = json.load(file)
+            except FileNotFoundError:
+                # If file doesn't exist, start with the new data
+                json_data = data
+            else:
+                # Update old data with new data
+                json_data.update(data)
+
+            finally:
+                ename.delete(0, END)
+                epassword.delete(0, END)
+            with open("./Password Generator/data.json", "w") as file:
+                #saving updated data
+                json.dump(json_data, file, indent=4)
     else:
         messagebox.showerror("Error", "Some field are empty!")
+
+def search_data():
+    name = ename.get()
+    try:
+        with open("./Password Generator/data.json") as file:
+            json_data = json.load(file)
+            if name in json_data:
+                messagebox.showinfo("Info", f"Name/website: {name}\nEmail: {json_data[name]['Email']}\nPassword: {json_data[name]['Password']}")
+            else:
+                messagebox.showerror("Error", f"Password has not generated for {name}")
+    except FileNotFoundError:
+        messagebox.showerror("Error", "File Not Found")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -52,10 +86,10 @@ email.grid(row=2,column=0)
 password = Label(window,text="Password:")
 password.grid(row=3,column=0)
 
-ename = Entry(window, width= 47)
+ename = Entry(window, width= 30)
 ename.focus()
 ename.grid(row=1, column=1, columnspan=2,sticky="w")
-eemail = Entry(window, width=47)
+eemail = Entry(window, width=30)
 eemail.grid(row=2, column=1,columnspan=2,sticky="w")
 epassword = Entry(window, width=30)
 epassword.grid(row=3,column=1,sticky="w")
@@ -64,5 +98,7 @@ generate = Button(window, text="Generate", width= 10, command=generate_password)
 generate.grid(row=3,column=2,sticky="w")
 add = Button(window, text="Add",width=40,command=save_data)
 add.grid(row=4,column=1,columnspan=2,sticky="w")
+search = Button(window, text="Search", width=10, command=search_data)
+search.grid(row=1, column=2,sticky="w")
 
 window.mainloop()
