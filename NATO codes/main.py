cfe9b6b88f85152bca114a3a0817d2d5e1877a72
Kundan <mauryakundan1612@gmail.com>
2026-04-01 15:50:33 +0530

    Refactor List function to clear previous results and improve code readability

--- NATO codes/main.py
+++ NATO codes/main.py
@@ -1,23 +1,20 @@
 import pandas
 import tkinter as tk
 
-
 student_data_frame = pandas.read_csv("./NATO codes/nato_phonetic_alphabet.csv")
-
-
 
 NATO = {row.letter: row.code for (index, row) in student_data_frame.iterrows()}
 
-
-# user = input("Enter a word to get phonetic: ").upper()
-
-# code = [ NATO[code] for code in user]
-# print(f"\n{code}")
 def List():
-    code = [NATO[letter] for letter in entry.get().upper() if letter in NATO]
-    for i in range(len(code)-1):
-        tk.Label(window, text=code[i]).pack()
-
+    for word in result:
+        word.pack_forget()
+    if entry.get().isalpha():
+        code = [NATO[letter] for letter in entry.get().upper() if letter in NATO]
+        for i in range(len(code)):
+            words = tk.Label(window, text=code[i])
+            words.pack()
+            result.append(words)
+    
 window = tk.Tk()
 window.minsize(300,300)
 
@@ -28,8 +25,6 @@ entry.pack()
 button = tk.Button(window, text="Get List", command=List)
 button.pack()
 
-
-
-
+result = []
 
 window.mainloop()
